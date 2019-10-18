28ac66e57b988cba202bdc002acca2fa07638610
alexfilipe <alex@afcbs.me>
2019-10-18 16:56:27 -0400

    add comments to integer_to_roman

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -1,6 +1,7 @@
 class Solution:
-    def intToRoman(self, num: int) -> str:
-        symbols = {
+    def __init__(self):
+        # Symbols representing units of Roman numerals up to 1000.
+        self.symbols = {
             1: 'I',
             4: 'IV',
             5: 'V',
@@ -16,20 +17,36 @@ class Solution:
             1000: 'M'
         }
 
-        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        # Integers representing remainders in Roman numerals up to 1000.
+        self.values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
+    def intToRoman(self, num: int) -> str:
+        """Returns Roman numeral representation of an integer up to 3999.
+
+        Args:
+            num (int): Integer to be converted.
+        Returns:
+            str: Roman numeral.
+        """
+
+        # Last remainder
         remainder = num
-        divisions = dict()
 
+        # Dict representing whole numbers of
+        divisions = dict.fromkeys(self.values)
+
+        # Initial string
         roman = ""
 
-        for v in values:
+        # Loops through all remainder values
+        for v in self.values:
             division = remainder // v
             remainder = remainder % v
 
             divisions[v] = division
 
+            # Adds to the string only if division is not empty.
             if divisions[v] != 0:
-                roman += symbols[v] * divisions[v]
+                roman += self.symbols[v] * divisions[v]
 
         return roman
